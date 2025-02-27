3bcb04b559abbb9a07d7e49a3c4f81d2c0bb5b5b
dmaseque <dmaseque@uci.edu>
2025-02-27 04:24:57 -0800

    create search for query and retrieval from inverted_index

---
+++ src/search.py
@@ -0,0 +1,84 @@
+import sys
+import json
+from indexer import inverted_index, tokenize
+
+#######################################################
+# TODO:
+# implement index offset from book keeping file
+# for now, it just loads entire final_index.json
+#######################################################
+def load_inverted_index(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"Error: '{file_path}' not found.")
+
+# search function
+# input is the query string
+def search(query):
+    # tokenize the query string
+    # output is list of (stemmed token, weight)
+    query_tokens_weight = tokenize(query, weight=1)
+    # get only the stemmed tokens => token[0] (first value of token)
+    query_stemmed_tokens = []
+    for token in query_tokens_weight:
+        query_stemmed_tokens.append(token[0])
+
+    # Initialize result as None, no documents
+    result = None
+
+    # for token in query of stemmed tokens, check for token in inverted index
+    for token in query_stemmed_tokens:
+        # take only unique docIDs for each token
+        token_docIDs = set()
+        # if token in inverted index, retrieve all the postings for that token
+        if token in inverted_index:
+            postings = inverted_index[token]
+            # for each posting, add the document ID
+            for posting in postings:
+#############################
+# TODO: need to map docID to document_name = want to output document name, not docID
+# right now, add document_name for testing, should be "document_id"
+#############################  
+                token_docIDs.add(posting["document_name"])      
+
+        # AND operation to get intersection of sets
+
+        # if there is nothing in result, add set of docIDs for token in query
+        if result is None:
+            result = token_docIDs
+        # if result contains docIDs, only add to result if docIDs in token_docIDs AND result
+        else:
+            result &= token_docIDs
+
+    # if result is empty, then no documents found in inverted index
+    if result == None:
+        return []
+    
+    # return result of list of document names (urls)
+    return result
+
+if __name__ == '__main__':
+    
+    # python search.py {query}
+    if len(sys.argv) < 2:
+        print("Invalid query.")
+        sys.exit(1)
+
+    # form the query string from command line arguments
+    query = " ".join(sys.argv[1:])
+
+    inverted_index = load_inverted_index("final_index.json")
+
+    if inverted_index:
+        search_result = search(query)
+        if search_result:
+            print("Documents found from query:")
+            for document in search_result:
+                print(document)
+        else:
+            # if search_result is empty, then no documents found in inverted index
+            print("No documents found.")
+    else:
+        print("Inverted Index is empty.")
